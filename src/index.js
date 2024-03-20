554075ed565aacf1d2c233944931b6460b96f02f
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-03-20 14:25:19 +1300

    Add classes to elements according to BEM methodology

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.scss";
+
+console.log(
+  [...document.querySelector("main").querySelectorAll("*")]
+    .map((element) => [...element.classList])
+    .flat()
+    .sort()
+    .filter((element, index, array) => element !== array[index + 1]),
+);
