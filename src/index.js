c44427108ecb4a0e7a3d0001eaf27e239f61c13c
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-03-22 12:35:24 +1300

    Create module for creating class lists

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import "./styles.scss";
+import classListModule from "./classListModule";
 
-console.log(
-  [...document.querySelector("main").querySelectorAll("*")]
-    .map((element) => [...element.classList])
-    .flat()
-    .sort()
-    .filter((element, index, array) => element !== array[index + 1]),
+const classes = classListModule.makeUniqueSet(".body", false);
+const heroClasses = classListModule.makeUniqueSet(".hero");
+const enjoyablePlaceClasses = classListModule.makeUniqueSet(".enjoyable-place");
+const locallySourcedClasses = classListModule.makeUniqueSet(".locally-sourced");
+const highlightsClasses = classListModule.makeUniqueSet(".highlights");
+const familyGatheringClasses =
+  classListModule.makeUniqueSet(".family-gathering");
+const ctaClasses = classListModule.makeUniqueSet(".cta");
+const footerClasses = classListModule.makeUniqueSet(".footer");
+
+const subSets = [
+  heroClasses,
+  enjoyablePlaceClasses,
+  locallySourcedClasses,
+  highlightsClasses,
+  familyGatheringClasses,
+  ctaClasses,
+  footerClasses,
+];
+
+const { uniqueSuperset, uniqueSubsets } = classListModule.makeSetsUnique(
+  classes,
+  subSets,
 );
+
+console.log(classes, uniqueSuperset);
+uniqueSubsets.forEach((set) => {
+  console.log(set);
+});
