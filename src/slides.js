685dd2fbd6b8ddcc2086326bec9d26e469d30a31
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-03-26 11:38:06 +1300

    Implement slide template and slide switching

--- src/slides.js
+++ src/slides.js
@@ -1,47 +1,85 @@
 import Handlebars from "handlebars";
 import patternTopRight from "./assets/patterns/pattern-curve-top-right.svg";
 import patternLines from "./assets/patterns/pattern-lines.svg";
-import image from "./assets/homepage/family-gathering-desktop@2x.jpg";
+import imageFamilyGathering from "./assets/homepage/family-gathering-desktop@2x.jpg";
+import imageSpecialEvents from "./assets/homepage/special-events-desktop@2x.jpg";
+import imageSocialEvents from "./assets/homepage/social-events-desktop@2x.jpg";
+import slides from "./slides.json";
 
 const slidesModule = (() => {
   // Cache DOM
   const mainElement = document.querySelector(".main");
+  const ctaElement = document.querySelector(".cta");
   const slidesTemplate = document.querySelector(".slides-template").innerHTML;
+  let slidesListElement = null;
+  let linePatternElement = null;
+  let activeSlideItemId = "list-item-family-gathering";
+  let activeSlideItemElement = null;
+  let slidesElement = null;
+
+  function createSlide(templateInputObject) {
+    slidesElement = elementFromString(
+      Handlebars.compile(slidesTemplate)(templateInputObject),
+    );
+
+    slidesListElement = slidesElement.querySelector(".slides__list");
+
+    linePatternElement = slidesElement.querySelector(".slides__line-pattern");
+
+    activeSlideItemElement = slidesElement.querySelector(
+      `#${activeSlideItemId}`,
+    );
+
+    activeSlideItemElement.dataset.active = "true";
+  }
 
   // Bind Events
-  mainElement.addEventListener("click", delegateEvent);
+  function bindEvents() {
+    slidesListElement.addEventListener("click", (event) => {
+      if (event.target.id === "") {
+        return;
+      }
 
-  function render() {
-    if (mainElement.querySelector(".slides")) {
-    }
+      activeSlideItemId = event.target.id;
 
-    // Get elements
-    const slidesElement = elementFromString(
-      Handlebars.compile(slidesTemplate)({
+      slidesElement.remove();
+
+      let image;
+      let slide;
+
+      switch (activeSlideItemId) {
+        case "list-item-family-gathering":
+          image = imageFamilyGathering;
+          slide = slides.familyGathering;
+          break;
+        case "list-item-special-events":
+          image = imageSpecialEvents;
+          slide = slides.specialEvents;
+          break;
+        case "list-item-social-events":
+          image = imageSocialEvents;
+          slide = slides.socialEvents;
+          break;
+        default:
+      }
+      createSlide({
         patternTopRight,
         patternLines,
         image,
-      }),
-    );
-    const buttonElement = slidesElement.querySelector(".button");
-    const linePatternElement = slidesElement.querySelector(
-      ".slides__line-pattern",
-    );
-    const listItemElement = slidesElement.querySelector(".slides__list");
+        ...slide,
+      });
 
-    listItemElement.addEventListener("click", (event) => {
-      console.log(event.target);
+      render();
     });
+  }
 
-    console.log(
-      slidesElement.querySelector(".slides__item[data-active='true']")
-        .textContent,
-    );
+  function render() {
+    // The line style will position the line to the left
+    // and vertical center of the active list item.
+    activeSlideItemElement.appendChild(linePatternElement);
+    mainElement.insertBefore(slidesElement, ctaElement);
 
-    slidesElement
-      .querySelector(".slides__item[data-active='true']")
-      .appendChild(linePatternElement);
-    mainElement.insertBefore(slidesElement, mainElement.children[4]);
+    bindEvents();
   }
 
   function elementFromString(html) {
@@ -51,19 +89,12 @@ const slidesModule = (() => {
     return element.firstChild;
   }
 
-  function delegateEvent(event) {
-    const target = event.target;
-    if (target.matches(".slides .slides__item[data-active='false']")) {
-      mainElement.querySelector(
-        ".slides__item[data-active='true']",
-      ).dataset.active = "false";
-      target.dataset.active = "true";
-    } else if (target.matches(".slides .button")) {
-    }
-
-    render();
-  }
-
+  createSlide({
+    patternTopRight,
+    patternLines,
+    image: imageFamilyGathering,
+    ...slides.familyGathering,
+  });
   render();
 })();
 
