841b84c97b6122b609ed2289165ecf9aeede426c
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-03-26 12:44:22 +1300

    Tidy index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
 import "./styles.scss";
-import classListModule from "./classListModule";
-import slidesModule from "./slides";
-
-// slidesModule.render();
-
-// const classes = classListModule.makeUniqueSet(".body", false);
-// const heroClasses = classListModule.makeUniqueSet(".hero");
-// const enjoyablePlaceClasses = classListModule.makeUniqueSet(".enjoyable-place");
-// const locallySourcedClasses = classListModule.makeUniqueSet(".locally-sourced");
-// const highlightsClasses = classListModule.makeUniqueSet(".highlights");
-// const familyGatheringClasses =
-//   classListModule.makeUniqueSet(".family-gathering");
-// const ctaClasses = classListModule.makeUniqueSet(".cta");
-// const footerClasses = classListModule.makeUniqueSet(".footer");
-
-// const subSets = [
-//   heroClasses,
-//   enjoyablePlaceClasses,
-//   locallySourcedClasses,
-//   highlightsClasses,
-//   familyGatheringClasses,
-//   ctaClasses,
-//   footerClasses,
-// ];
-
-// const { uniqueSuperset, uniqueSubsets } = classListModule.makeSetsUnique(
-//   classes,
-//   subSets,
-// );
-
-// console.log(classes, uniqueSuperset);
-// uniqueSubsets.forEach((set) => {
-//   console.log(set);
-// });
+import "./classListModule";
+import "./slides";
