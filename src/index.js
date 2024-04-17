33e61b3238b854dec04bef556042eac87ee90bbf
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-17 18:12:53 +1200

    Add skeleton for the booking section

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import "./styles.scss";
 import "./classListModule";
 import "./slides";
 
+const mainElement = document.querySelector(".main");
+const bookingElement = document.querySelector(".booking-page");
+
+bookingElement.style.display = "none";
+
 [...document.querySelectorAll("[data-action='book-table']")].forEach(
   (button) => {
-    button.addEventListener("click", (event) => {});
+    button.addEventListener("click", (event) => {
+      bookingElement.style.display = "flow";
+      mainElement.style.display = "none";
+    });
   },
 );
