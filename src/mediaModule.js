7932c1991279066ab6e52574c4cf892a5263ddfd
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-18 20:26:37 +1200

    Style the tablet locally sourced section

--- src/mediaModule.js
+++ src/mediaModule.js
@@ -4,12 +4,18 @@ import heroImageMobile from "./assets/homepage/hero-bg-mobile@2x.jpg";
 import enjoyablePlaceImageDesktop from "./assets/homepage/enjoyable-place-desktop@2x.jpg";
 import enjoyablePlaceImageTablet from "./assets/homepage/enjoyable-place-tablet@2x.jpg";
 import enjoyablePlaceImageMobile from "./assets/homepage/enjoyable-place-mobile@2x.jpg";
+import locallySourcedImageDesktop from "./assets/homepage/locally-sourced-desktop@2x.jpg";
+import locallySourcedImageTablet from "./assets/homepage/locally-sourced-tablet@2x.jpg";
+import locallySourcedImageMobile from "./assets/homepage/locally-sourced-mobile@2x.jpg";
 
 const mediaModules = (() => {
   // cache DOM
   const heroImageElement = document.querySelector(".hero__image");
   const enjoyablePlaceImageElement = document.querySelector(
     ".enjoyable-place__image",
+  );
+  const locallySourcedImageElement = document.querySelector(
+    ".locally-sourced__image",
   );
 
   const tablet = matchMedia("(max-width: 1100px)");
@@ -23,12 +29,15 @@ const mediaModules = (() => {
     if (mobile.matches) {
       heroImageElement.src = heroImageMobile;
       enjoyablePlaceImageElement.src = enjoyablePlaceImageDesktop;
+      locallySourcedImageElement.src = locallySourcedImageDesktop;
     } else if (tablet.matches) {
       heroImageElement.src = heroImageTablet;
       enjoyablePlaceImageElement.src = enjoyablePlaceImageTablet;
+      locallySourcedImageElement.src = locallySourcedImageTablet;
     } else {
       heroImageElement.src = heroImageDesktop;
       enjoyablePlaceImageElement.src = enjoyablePlaceImageMobile;
+      locallySourcedImageElement.src = locallySourcedImageMobile;
     }
   }
 
