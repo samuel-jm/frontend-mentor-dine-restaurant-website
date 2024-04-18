08f3fe1137c1698d58d4598d66151efa629109b0
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-18 20:38:02 +1200

    Style the tablet highlights section

--- src/mediaModule.js
+++ src/mediaModule.js
@@ -7,6 +7,12 @@ import enjoyablePlaceImageMobile from "./assets/homepage/enjoyable-place-mobile@
 import locallySourcedImageDesktop from "./assets/homepage/locally-sourced-desktop@2x.jpg";
 import locallySourcedImageTablet from "./assets/homepage/locally-sourced-tablet@2x.jpg";
 import locallySourcedImageMobile from "./assets/homepage/locally-sourced-mobile@2x.jpg";
+import salmonImageDesktopTablet from "./assets/homepage/salmon-desktop-tablet@2x.jpg";
+import salmonImageMobile from "./assets/homepage/salmon-mobile@2x.jpg";
+import beefImageDesktopTablet from "./assets/homepage/beef-desktop-tablet@2x.jpg";
+import beefImageMobile from "./assets/homepage/beef-mobile@2x.jpg";
+import chocolateImageDesktopTablet from "./assets/homepage/chocolate-desktop-tablet@2x.jpg";
+import chocolateImageMobile from "./assets/homepage/chocolate-mobile@2x.jpg";
 
 const mediaModules = (() => {
   // cache DOM
@@ -17,6 +23,9 @@ const mediaModules = (() => {
   const locallySourcedImageElement = document.querySelector(
     ".locally-sourced__image",
   );
+  const salmonImageElement = document.querySelector("[salmon]");
+  const beefImageElement = document.querySelector("[beef]");
+  const chocolateImageElement = document.querySelector("[chocolate]");
 
   const tablet = matchMedia("(max-width: 1100px)");
   const mobile = matchMedia("(max-width: 0px)");
@@ -28,16 +37,25 @@ const mediaModules = (() => {
   function onMediaChange() {
     if (mobile.matches) {
       heroImageElement.src = heroImageMobile;
-      enjoyablePlaceImageElement.src = enjoyablePlaceImageDesktop;
-      locallySourcedImageElement.src = locallySourcedImageDesktop;
+      enjoyablePlaceImageElement.src = enjoyablePlaceImageMobile;
+      locallySourcedImageElement.src = locallySourcedImageMobile;
+      salmonImageElement.src = salmonImageMobile;
+      beefImageElement.src = beefImageMobile;
+      chocolateImageElement.src = chocolateImageMobile;
     } else if (tablet.matches) {
       heroImageElement.src = heroImageTablet;
       enjoyablePlaceImageElement.src = enjoyablePlaceImageTablet;
       locallySourcedImageElement.src = locallySourcedImageTablet;
+      salmonImageElement.src = salmonImageDesktopTablet;
+      beefImageElement.src = beefImageDesktopTablet;
+      chocolateImageElement.src = chocolateImageDesktopTablet;
     } else {
       heroImageElement.src = heroImageDesktop;
-      enjoyablePlaceImageElement.src = enjoyablePlaceImageMobile;
-      locallySourcedImageElement.src = locallySourcedImageMobile;
+      enjoyablePlaceImageElement.src = enjoyablePlaceImageDesktop;
+      locallySourcedImageElement.src = locallySourcedImageDesktop;
+      salmonImageElement.src = salmonImageDesktopTablet;
+      beefImageElement.src = beefImageDesktopTablet;
+      chocolateImageElement.src = chocolateImageDesktopTablet;
     }
   }
 
