576a35a7543d13bcd3481ae03670d24596de2723
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-22 11:40:52 +1200

    Create a button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let text;
+	export let style;
+</script>
+
+<input type="button" value={text} {style} />
+
+<style lang="scss">
+	input {
+		border: none;
+		position: var(--position);
+
+		background-color: var(--bg, black);
+		border-radius: 0px;
+		border: 1px solid white;
+		padding: 25px 55px;
+
+		color: white;
+
+		&:hover {
+			background-color: white;
+			border: var(--border-hover, 1px solid black);
+
+			color: var(--colour-cod-gray);
+		}
+	}
+</style>
