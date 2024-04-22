822f6942602ca5d9ff8b5e97d9fb78e0b7565a48
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-23 10:03:35 +1200

    Create the booking Page component

---
+++ src/routes/booking/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Hero from './Hero.svelte';
+	import Filler from './Filler.svelte';
+
+	export let data;
+</script>
+
+<Hero --horizontal-padding={data.horizontalPadding} />
+<Filler />
