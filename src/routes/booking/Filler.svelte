8c68a2ad9a5698bc02911ead27a9deb7108263eb
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-23 10:03:10 +1200

    Create the Filler component

---
+++ src/routes/booking/Filler.svelte
@@ -0,0 +1,25 @@
+<section>
+	<img id="pattern-br" src="/patterns/pattern-curve-bottom-right.svg" alt="" />
+	<img id="pattern-lines" src="/patterns/pattern-lines.svg" alt="" />
+</section>
+
+<style lang="scss">
+	section {
+		height: 319px;
+
+		& > img {
+			height: auto;
+		}
+
+		#pattern-br {
+			position: absolute;
+		}
+
+		#pattern-lines {
+			position: absolute;
+
+			right: 625px;
+			top: 166px;
+		}
+	}
+</style>
