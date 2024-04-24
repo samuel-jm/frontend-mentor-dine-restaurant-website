8c6a4969a1fa8b01877ed88e6d34afbce28d482e
samuel-jm <114629227+samuel-jm@users.noreply.github.com>
2024-04-24 14:17:05 +1200

    Style the tablet layout Overview component

--- src/routes/Overview.svelte
+++ src/routes/Overview.svelte
@@ -140,4 +140,120 @@
 		letter-spacing: var(--letter-spacing-zero);
 		line-height: var(--line-height-m);
 	}
+
+	@media (max-width: 0px) {
+	}
+
+	@media (max-width: 1000px) {
+		#pattern-tr {
+			left: -517px;
+			top: 100px;
+		}
+
+		#pattern-tl {
+			right: -511px;
+		}
+
+		#pattern-lines {
+			bottom: 489px;
+			right: 40px;
+
+			z-index: 1;
+		}
+
+		section {
+			display: grid;
+			grid-template-columns: 1fr;
+			grid-template-rows: repeat(4, 1fr);
+			grid-template-areas:
+				'enjoyable-place-img'
+				'enjoyable-place-text'
+				'locally-sourced-img'
+				'locally-sourced-text';
+
+			height: 1490px;
+		}
+
+		.crop {
+			place-self: center;
+			transform: translateY(0px);
+
+			position: relative;
+
+			aspect-ratio: 573 / 360;
+			width: min(573px, 100%);
+			margin-bottom: -10px;
+		}
+
+		.crop,
+		#enjoyable-place-text {
+			transform: translateY(-112px);
+		}
+
+		#enjoyable-place-img {
+			position: static;
+
+			content: url('/homepage/enjoyable-place-tablet@2x.jpg');
+			width: 100%;
+			height: auto;
+		}
+
+		#enjoyable-place-text {
+			place-self: center;
+
+			margin-left: 0;
+			max-width: 48ch;
+
+			text-align: center;
+		}
+
+		#locally-sourced-img,
+		#locally-sourced-text {
+			transform: translateY(-62px);
+		}
+
+		#locally-sourced-img {
+			place-self: center;
+			position: relative;
+
+			content: url('/homepage/locally-sourced-tablet@2x.jpg');
+			width: min(573px, 100%);
+
+			margin-bottom: -10px;
+		}
+
+		#locally-sourced-text {
+			place-self: center;
+			grid-area: locally-sourced-text;
+
+			margin-right: 0;
+			max-width: 45ch;
+
+			text-align: center;
+		}
+
+		h2,
+		p {
+			color: var(--colour-ebony-clay);
+
+			font-family: 'League Spartan';
+		}
+
+		h2 {
+			margin-top: 39px;
+			margin-bottom: 27px;
+
+			font-size: var(--size-2xl);
+			font-weight: var(--weight-bold);
+			letter-spacing: var(--letter-spacing-xs);
+			line-height: var(--line-height-xl);
+		}
+
+		p {
+			font-size: var(--size-l);
+			font-weight: var(--weight-regular);
+			letter-spacing: var(--letter-spacing-zero);
+			line-height: var(--line-height-m);
+		}
+	}
 </style>
